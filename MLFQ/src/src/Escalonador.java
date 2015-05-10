6bc716b6777b1055b5fa1abc34e9641813c47260
Rafael Paiva <08.paiva@gmail.com>
2015-05-09 21:12:19 -0300

    Movi para o pacote src...

--- MLFQ/src/src/Escalonador.java
+++ MLFQ/src/src/Escalonador.java
@@ -1,8 +1,10 @@
+package src;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package src;
+
 
 import java.util.ArrayList;
 import java.util.List;
